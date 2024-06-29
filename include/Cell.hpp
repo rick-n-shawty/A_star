06bdb54eb375237242d455c7a10bc863f33f9e42
rick-n-shawty <sultanhoodie@gmail.com>
2024-06-29 14:44:55 +0800

    first commit

---
+++ include/Cell.hpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+#ifndef CELL_HPP 
+#define CELL_HPP 
+
+class Cell{
+    private: 
+        sf::RectangleShape shape; 
+    public: 
+        Cell(); 
+        ~Cell(); 
+};
+
+#endif 
